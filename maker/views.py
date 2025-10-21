ffc009846fb64ada607690b65867d524203031e8
Jan Lindblad <jan.lindblad@for.eco>
2025-10-21 10:31:47 +0200

    Make success/warning/error messages popup in the upper right corner, always visible

--- maker/views.py
+++ maker/views.py
@@ -10,7 +10,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.core.exceptions import ValidationError
-from django.contrib import messages
 from django.db import models
 import json
 
